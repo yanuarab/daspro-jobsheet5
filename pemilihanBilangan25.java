c847c193f1feb338cb915a3dbb7f1a6e2efdcd42
tugas <aldayanuar6@gmail.com>
2024-09-27 10:45:01 +0700

    praktikum05

--- pemilihanBilangan25.java
+++ pemilihanBilangan25.java
@@ -4,6 +4,7 @@ public class pemilihanBilangan25 {
         Scanner sc = new Scanner(System.in);
         System.out.println("Masukan sebuah angka: ");
         int angka = sc.nextInt();
+        
         if (angka % 2 == 0)
         {
             System.out.println("Angka: " + angka + " Termasuk bilangan genap");
