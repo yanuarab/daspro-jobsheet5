bf3b9cee9fdda103b529546183f2dac674e29b23
tugas <aldayanuar6@gmail.com>
2024-09-27 10:17:21 +0700

    jobsheet5

---
+++ pemilihanBilangan25.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+public class pemilihanBilangan25 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Masukan sebuah angka: ");
+        int angka = sc.nextInt();
+        if (angka % 2 == 0)
+        {
+            System.out.println("Angka: " + angka + " Termasuk bilangan genap");
+        }
+        else
+        {
+            System.out.println("Angka: " + angka + " Termasuk bilangan ganjil");
+        }
+        
+    }
+    
+}
---
+++ pemilihanSwitch25.java
@@ -0,0 +1,32 @@
+import java.util.Scanner;
+public class pemilihanSwitch25 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        double angka1, angka2, hasil = 0;
+        char operator;
+        System.out.println("Masukan angka 1: ");
+        angka1 = sc.nextDouble();
+        System.out.println("Masukan angka 2: ");
+        angka2 = sc.nextDouble();
+        System.out.println("Masukan operator (+ - * :)");
+        operator = sc.next().charAt(0);
+        switch (operator) {
+            case '+' :
+                hasil = angka1 + angka2;
+                break;
+            case '-' :
+                hasil = angka1 - angka2;
+                break;
+            case '*' :
+                hasil = angka1 * angka2;
+                break;
+            case '/' :
+                hasil = angka1 / angka2;
+                break;
+
+            
+        }
+        System.out.println(angka1 + operator + angka2 + "=" + hasil);
+    }
+    
+}
